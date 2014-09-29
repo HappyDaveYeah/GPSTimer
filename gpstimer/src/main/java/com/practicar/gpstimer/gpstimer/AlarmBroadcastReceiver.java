cbfe3e546a5b0d35c112eb064156e571f1bec759
HappyDaveYeah <davidsantosp@gmail.com>
2014-09-29 18:12:07 +0200

    Petits fixes d'estructura

--- gpstimer/src/main/java/com/practicar/gpstimer/gpstimer/AlarmBroadcastReceiver.java
+++ gpstimer/src/main/java/com/practicar/gpstimer/gpstimer/AlarmBroadcastReceiver.java
@@ -10,19 +10,18 @@ import android.widget.Toast;
 /**
  * Created by HappyDave on 24/07/2014.
  */
-public class AlarmReceiver extends BroadcastReceiver{
+public class AlarmBroadcastReceiver extends BroadcastReceiver{
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        Toast.makeText(context, "Congrats!. Your Alarm time has been reached", Toast.LENGTH_LONG).show();
+        Toast.makeText(context, "Congrats!", Toast.LENGTH_LONG).show();
     }
 
     public void setTimer(Context context) {
         AlarmManager alarmManager = (AlarmManager)context.getSystemService(Context.ALARM_SERVICE);
-        Intent intent = new Intent(context, AlarmReceiver.class);
-        //intent.putExtra(ONE_TIME, Boolean.TRUE);
+        Intent intent = new Intent(context, AlarmBroadcastReceiver.class);
         PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 0, intent, 0);
-        alarmManager.set(AlarmManager.RTC_WAKEUP, System.currentTimeMillis() + 5000, pendingIntent);
+        alarmManager.set(AlarmManager.RTC_WAKEUP, System.currentTimeMillis() + 2000, pendingIntent);
 
     }
 }
---
+++ gpstimer/src/main/java/com/practicar/gpstimer/gpstimer/GMapFragment.java
@@ -0,0 +1,48 @@
+package com.practicar.gpstimer.gpstimer;
+
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+
+public class GMapFragment extends Fragment {
+    private GoogleMap map;
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_gmap, container, false);
+
+        GoogleMap map = ((SupportMapFragment) getFragmentManager()
+                .findFragmentById(R.id.map)).getMap();
+
+        LatLng sydney = new LatLng(-33.867, 151.206);
+
+        map.setMyLocationEnabled(true);
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 13));
+
+        map.addMarker(new MarkerOptions()
+                .title("Sydney")
+                .snippet("The most populous city in Australia.")
+                .position(sydney));
+
+        return view;
+    }
+
+
+}
